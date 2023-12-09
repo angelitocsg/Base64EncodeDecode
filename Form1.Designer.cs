2b4d0683a78f5517a544e0ffa756df5084e2c8d9
Angelito Casagrande <angelito@angelito.com.br>
2023-12-08 22:51:37 -0300

    base project

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Base64EncodeDecode
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.rdEncode = new System.Windows.Forms.RadioButton();
+            this.rdDecode = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(524, 162);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtOutput.Location = new System.Drawing.Point(12, 180);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(524, 181);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // rdEncode
+            // 
+            this.rdEncode.AutoSize = true;
+            this.rdEncode.Checked = true;
+            this.rdEncode.Location = new System.Drawing.Point(6, 17);
+            this.rdEncode.Name = "rdEncode";
+            this.rdEncode.Size = new System.Drawing.Size(64, 19);
+            this.rdEncode.TabIndex = 3;
+            this.rdEncode.TabStop = true;
+            this.rdEncode.Text = "&Encode";
+            this.rdEncode.UseVisualStyleBackColor = true;
+            // 
+            // rdDecode
+            // 
+            this.rdDecode.AutoSize = true;
+            this.rdDecode.Location = new System.Drawing.Point(76, 17);
+            this.rdDecode.Name = "rdDecode";
+            this.rdDecode.Size = new System.Drawing.Size(65, 19);
+            this.rdDecode.TabIndex = 4;
+            this.rdDecode.Text = "&Decode";
+            this.rdDecode.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdEncode);
+            this.groupBox1.Controls.Add(this.rdDecode);
+            this.groupBox1.Location = new System.Drawing.Point(12, 367);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(150, 45);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Action";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 424);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Base64 - Encode | Decode";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.RadioButton rdEncode;
+        private System.Windows.Forms.RadioButton rdDecode;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Base64EncodeDecode
+{
+    public partial class Form1 : Form
+    {
+        public enum Action
+        {
+            Encode,
+            Decode
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            Action action = rdEncode.Checked ? Action.Encode : Action.Decode;
+
+            switch (action)
+            {
+                case Action.Encode:
+                    txtOutput.Text = EncodeToBase64(txtInput.Text);
+                    break;
+
+                case Action.Decode:
+                    txtOutput.Text = DecodeFrom64(txtInput.Text);
+                    break;
+            }
+        }
+
+        private string EncodeToBase64(string text)
+        {
+            try
+            {
+                byte[] textAsBytes = Encoding.ASCII.GetBytes(text);
+                return Convert.ToBase64String(textAsBytes);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        private string DecodeFrom64(string data)
+        {
+            try
+            {
+                byte[] dataAsBytes = Convert.FromBase64String(data);
+                return Encoding.ASCII.GetString(dataAsBytes);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
